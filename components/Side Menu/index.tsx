1829361431d490e6a0df1b9a8a128fb86eef46b4
Aidan <datbluemushroom@gmail.com>
2023-07-12 02:52:40 -0400

    ✨ Basic Side Menu

--- components/Side Menu/index.tsx
+++ components/Side Menu/index.tsx
@@ -1,15 +1,28 @@
+import { Home, CalendarPlus, Settings } from 'lucide-react';
+
 export const SideMenu = () => {
     return (
         <>
-            <div className="flex flex-col w-1/5 bg-orange-400 items-center justify-start">
+            <div className="flex flex-col w-1/5 bg-orange-400 justify-start items-center">
                 
                 {/* Logo */}
-                <div className="w-20 h-20 bg-black m-10"/>
+                <div className="w-20 h-20 bg-white m-10"/>
 
                 {/* Nav Buttons */}
-                <button className="w-full h-16 bg-orange-400 transition-all duration-300 hover:bg-orange-500">Home</button>
-                <button className="w-full h-16 bg-orange-400 transition-all duration-300 hover:bg-orange-500">Days</button>
-                <button className="w-full h-16 bg-orange-400 transition-all duration-300 hover:bg-orange-500">Settings</button>
+                <button className="flex flex-row w-full h-16 text-white text-xl font-bold uppercase bg-orange-400 justify-center items-center gap-3 transition-all duration-300 hover:bg-orange-500">
+                    <Home size={36}/>
+                    <h1 className='hidden md:inline'>Home</h1>
+                </button>
+
+                <button className="flex flex-row w-full h-16 text-white text-xl font-bold uppercase bg-orange-400 justify-center items-center gap-3 transition-all duration-300 hover:bg-orange-500">
+                    <CalendarPlus size={36}/>
+                    <h1 className='hidden md:inline'>Days</h1>
+                </button>
+
+                <button className="flex flex-row w-full h-16 text-white text-xl font-bold uppercase bg-orange-400 justify-center items-center gap-3 transition-all duration-300 hover:bg-orange-500">
+                    <Settings size={36}/>
+                    <h1 className='hidden md:inline'>Settings</h1>
+                </button>
 
             </div>
         </>
