78e6ec57db27fea5c4142df727fafe15e5837112
Aidan <datbluemushroom@gmail.com>
2023-07-14 16:22:03 -0400

    working side menu selected tab

---
+++ app/settings/page.tsx
@@ -0,0 +1,11 @@
+export default function Settings() {
+    return (
+      <main className="flex w-full min-h-screen flex-col items-center justify-between p-24 bg-red-500">
+        <div className="flex flex-col z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex bg-blue-500">
+          
+          <h1>hi</h1>
+  
+        </div>
+      </main>
+    )
+  }
--- components/Side Menu/index.tsx
+++ components/Side Menu/index.tsx
@@ -1,6 +1,34 @@
+'use client'
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
 import { Home, CalendarPlus, Settings } from 'lucide-react';
 
 export const SideMenu = () => {
+
+    // tabs for the nav bar
+    const options = [
+        {
+            name: "home",
+            icon: <Home size={36} />,
+        }, 
+        {
+            name: "days", 
+            icon: <CalendarPlus size={36} />,
+        },
+        {
+            name: "settings",
+            icon: <Settings size={36} />,
+        },
+    ];
+
+    // get the current route
+    const currentRoute = usePathname();
+
+    const inactiveStyle = "flex flex-row w-full h-16 bg-orange-400 justify-center items-center gap-3 transition-all duration-300 hover:bg-orange-500"
+    const activeStyle = "flex flex-row w-full h-16 bg-orange-500 justify-center items-center gap-3 transition-all duration-300 hover:bg-orange-500"
+
     return (
         <>
             <div className="flex flex-col w-1/5 bg-orange-400 justify-start items-center">
@@ -9,20 +37,16 @@ export const SideMenu = () => {
                 <div className="w-20 h-20 bg-white m-10"/>
 
                 {/* Nav Buttons */}
-                <button className="flex flex-row w-full h-16 text-white text-xl font-bold uppercase bg-orange-400 justify-center items-center gap-3 transition-all duration-300 hover:bg-orange-500">
-                    <Home size={36}/>
-                    <h1 className='hidden md:inline'>Home</h1>
-                </button>
-
-                <button className="flex flex-row w-full h-16 text-white text-xl font-bold uppercase bg-orange-400 justify-center items-center gap-3 transition-all duration-300 hover:bg-orange-500">
-                    <CalendarPlus size={36}/>
-                    <h1 className='hidden md:inline'>Days</h1>
-                </button>
-
-                <button className="flex flex-row w-full h-16 text-white text-xl font-bold uppercase bg-orange-400 justify-center items-center gap-3 transition-all duration-300 hover:bg-orange-500">
-                    <Settings size={36}/>
-                    <h1 className='hidden md:inline'>Settings</h1>
-                </button>
+                {
+                    options.map((option) => (
+                        <Link className="w-full" href={option.name === "home" ? ("/") : (option.name)}>
+                            <button className={((currentRoute === "/" && option.name === "home") || (currentRoute === "/" + option.name)) ? (activeStyle) : (inactiveStyle)}>
+                                {option.icon}
+                                <h1 className='text-white text-xl font-bold uppercase hidden md:inline'>{option.name}</h1>
+                            </button>
+                        </Link>
+                    ))
+                }
 
             </div>
         </>
