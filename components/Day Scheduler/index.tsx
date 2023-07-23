bbad3206ab4e364f4c5a81fdd8e3a62ebf6f2055
Aidan <datbluemushroom@gmail.com>
2023-07-23 01:48:20 -0400

    ✨ location + description for appointments

--- components/Day Scheduler/index.tsx
+++ components/Day Scheduler/index.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { useState, MouseEvent, useEffect } from "react";
+import { X, Trash2, Clock, MapPin, AlignLeft } from 'lucide-react';
 
 export const DayScheduler = () => {
 
@@ -22,13 +23,15 @@ export const DayScheduler = () => {
         start: string; end: string 
     }>();
     const [state, setState] = useState<{
-        appointments: { [key: string]: { start: string; end: string } };
+        appointments: { [key: string]: { start: string; end: string, location: string, description: string } };
         startTimes: string[];
     }>({
         appointments: {
             "0:00": {
                 start: "0:00",
                 end: "1:00",
+                location: "Somewhere, US",
+                description: "Test appointment."
             }
         },
         startTimes: [
@@ -46,10 +49,12 @@ export const DayScheduler = () => {
             const startTime = times[key];
             const endTime = key != times.length - 1 ? times[key + 1] : times[0];
 
-            const newAppointment: { [appointmentKey: string]: { start: string, end: string } } = {};
+            const newAppointment: { [appointmentKey: string]: { start: string; end: string, location: string, description: string } } = {};
             newAppointment[startTime] = {
                 start: startTime,
                 end: endTime,
+                location: "",
+                description: "",
             };
 
             addAppointment(newAppointment);
@@ -58,7 +63,7 @@ export const DayScheduler = () => {
         }
     }
 
-    const addAppointment = (appointment: { [key: string]: { start: string; end: string } }) => {
+    const addAppointment = (appointment: { [key: string]: { start: string; end: string, location: string, description: string } }) => {
         const startTime = Object.keys(appointment)[0]; // Extract the start time from the keys of the appointment object
         const appointmentContent = appointment[startTime];
     
@@ -114,6 +119,14 @@ export const DayScheduler = () => {
         clearSelectedAppointment();
     }
 
+    const saveSelectedAppointment = () => {
+        if (!selected) return;
+        const appointmentKey = selected.start;
+
+        // TODO:    save appointment information on selected form to state
+        // TODO:    and update state
+    }
+
     useEffect(() => {
         selected ? (selectAppointment(selected.start)) : (null)
     }, [selected])
@@ -149,22 +162,45 @@ export const DayScheduler = () => {
                         selected ? (
                             <>
 
-                                <div className="flex flex-row justify-between items-center w-full h-24">
-                                    <button onClick={clearSelectedAppointment}>X</button>
+                                <div className="flex flex-row justify-between items-center w-full h-24 px-5">
+                                    <X className="cursor-pointer" onClick={clearSelectedAppointment}/>
                                     <input className="w-1/2 h-12 bg-gray-200 border-b-gray-500 border-b-2 text-gray-500 text-lg font-bold p-3 focus:outline-none"/>
-                                    <button onClick={deleteSelectedAppointment}>Delete</button>
+                                    <Trash2 className="cursor-pointer" onClick={deleteSelectedAppointment}/>
                                 </div>
-                                <div className="flex flex-col w-full h-full px-24 py-10">
+
+                                <div className="flex flex-col w-full h-full px-24 py-10 gap-10">
                                     <div className="flex flex-row items-center w-full h-16 gap-4">
+                                        <Clock />
                                         <input 
-                                            className="flex text-center w-28 h-full rounded-2xl focus:outline-none" 
+                                            className="flex text-center w-1/4 h-full rounded-2xl focus:outline-none" 
                                             placeholder={selected.start} 
                                         />
                                         <h1>to</h1>
                                         <input 
-                                            className="flex text-center w-28 h-full rounded-2xl focus:outline-none"
+                                            className="flex text-center w-1/4 h-full rounded-2xl focus:outline-none"
                                             placeholder={selected.end} 
                                         />
+                                    </div>
+
+                                    <div className="flex flex-row items-center w-full h-16 gap-4">
+                                        <MapPin />
+                                        <input 
+                                            className="flex w-full h-full rounded-2xl p-8 focus:outline-none"
+                                            placeholder={""} 
+                                        />
+                                    </div>
+
+                                    <div className="flex flex-row items-start w-full h-16 gap-4">
+                                        <AlignLeft />
+                                        <input 
+                                            className="flex w-full h-full rounded-2xl p-8 focus:outline-none"
+                                            placeholder={""} 
+                                        />
+                                    </div>
+
+                                    <div className="flex flex-row justify-evenly items-center w-full h-16 gap-4">
+                                        <button>Save</button>
+                                        <button>Cancel</button>
                                     </div>
                                 </div>
                             
